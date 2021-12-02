aad62709c5a15958917698a0196c1413db40892a
Yasho <90166733+Yasho022@users.noreply.github.com>
2021-12-02 18:42:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ inputBox.addEventListener('keyup', function(event) {
 });
 
 function getWeather() {
-	const apiKey = process.env.API_KEY;
+	const apiKey = "b37dcd068f23d5bbd5e055ba7392243a";
 	if (inputBox.value == '') {
 		inputBox.classList.add('shake');
 		setTimeout(function() {
